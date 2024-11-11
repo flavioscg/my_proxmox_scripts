af6032bd346f8d96fc398f2149be6d040eb3fc8b
Flavio Scognamiglio <67764234+flavioscg@users.noreply.github.com>
2024-11-11 16:03:43 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,6 +0,0 @@
-(New-Object net.webclient).DownloadFile('https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1',"$([Environment]::GetFolderPath("Desktop"))\install.ps1")
-Unblock-File .\flarevm.ps1
-
-Set-ExecutionPolicy Unrestricted -Force
-
-.\install.ps1 -noWait -noGui -noPassword
