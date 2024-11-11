8b2b31a2a6d782d410c0ef44bfc27fa34bfd5352
Flavio Scognamiglio <67764234+flavioscg@users.noreply.github.com>
2024-11-11 13:59:36 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,17 +1,5 @@
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" -Name ExecutionPolicy -Value Unrestricted
-
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableRealtimeMonitoring" -PropertyType DWord -Value 1
-
-Stop-Service -Name "wuauserv" -Force
-Set-Service -Name "wuauserv" -StartupType Disabled
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -PropertyType DWord -Value 1
-
-Write-Output "Windows Defender e Windows Update sono stati disabilitati con successo."
-
 (New-Object net.webclient).DownloadFile('https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1',"$([Environment]::GetFolderPath("Desktop"))\install.ps1")
-Unblock-File .\install.ps1
+Unblock-File .\flarevm.ps1
 
 Set-ExecutionPolicy Unrestricted -Force
 
